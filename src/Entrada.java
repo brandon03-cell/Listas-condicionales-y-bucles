e4ab2ab2824b5a8631f26eb903ee0d9c7a64f735
brandon03-cell <brandonchusgo2003@gmail.com>
2026-02-09 20:41:10 +0100

    miau

--- src/Entrada.java
+++ src/Entrada.java
@@ -1,4 +1,47 @@
+import java.util.Objects;
+
 public class Entrada {
     private Concierto concierto;
-    public Enum {PISTA, GRADA, VIP};
+    public enum Tipo {
+        PISTA,
+        GRADA,
+        VIP};
+    private Tipo tipo;
+
+    public Entrada(Concierto concierto, Tipo tipo) {
+        this.concierto = concierto;
+        this.tipo = tipo;
+    }
+
+    public Entrada() {
+    }
+
+    public Concierto getConcierto() {
+        return concierto;
+    }
+
+    public void setConcierto(Concierto concierto) {
+        this.concierto = concierto;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Entrada entrada = (Entrada) o;
+        return Objects.equals(getConcierto(), entrada.getConcierto()) && getTipo() == entrada.getTipo();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getConcierto(), getTipo());
+    }
+
 }
